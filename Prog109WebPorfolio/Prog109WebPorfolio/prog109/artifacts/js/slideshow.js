b0d5a0e2b83eff33363d97a1ef176da06f8aa730
bcjamuna <jamuna.blumette@bellevuecollege.edu>
2015-11-21 03:54:18 -0800

    completed slideshow

--- Prog109WebPorfolio/Prog109WebPorfolio/prog109/artifacts/js/slideshow.js
+++ Prog109WebPorfolio/Prog109WebPorfolio/prog109/artifacts/js/slideshow.js
@@ -1,8 +1,11 @@
-﻿var listImages = ["http://www.animalfactsencyclopedia.com/images/animalextremeclose-up-dolphin.jpg",
-  "http://cdn2.teen.com/wp-content/gallery/baby-dolphin-pictures/baby-dolphin-pictures-9.jpg",
-  "http://newshour-tc.pbs.org/newshour/wp-content/uploads/2014/03/dolphin.jpg"];
+﻿var listImages = ["https://d1tijy5l7mg5kk.cloudfront.net/assets/custom/review/2014/2014-javascript-road-trip-part-1-e04efc79c465b890f6b9680abf3f44b5.png",
+  "http://www.ginger-cookielaw.com/wp-content/uploads/2015/07/js-logo-badge-512.png",
+   "http://localhost:64688/../../images/looping.png",
+    "https://achievement-images.teamtreehouse.com/javascript-loops-stage3.png",
+"https://assets-cdn.github.com/images/modules/logos_page/Octocat.png"];
+   
 
-var captionImage = ["Live", "Love", "Laugh"];
+var captionImage = ["CSS 3 & HTML 5", "JavaScript", "Web Development", "JavaScript Loops", "GitHub"];
 
 
 var i = 0;
@@ -10,14 +13,14 @@ var i = 0;
 //Way to assign the onclick action to a button
 
 document.getElementById("next").onclick = Next;
-
+document.getElementById("previous").onclick = Previous;
 
 window.setInterval(showImages, 2000);
 
 function showImages() {
     if (document.getElementById("auto").checked) {
         if (i < captionImage.length) {
-            document.getElementById("dolphin").src = listImages[i];
+            document.getElementById("htmlcss").src = listImages[i];
             document.getElementById("captions").textContent = captionImage[i];
             i++;
         }
@@ -34,12 +37,24 @@ function Next() {
     else
         i = 0;
 
-    document.getElementById("dolphin").src = listImages[i];
+    document.getElementById("htmlcss").src = listImages[i];
     document.getElementById("captions").textContent = captionImage[i];
 
 }
 
 
+//Previous function
+
+function Previous() {
+    if (i > 0)
+        i--;
+    else
+        i = 4;
+
+    document.getElementById("htmlcss").src = listImages[i];
+    document.getElementById("captions").textContent = captionImage[i];
+
+}
 
 
 
---
+++ Prog109WebPorfolio/Prog109WebPorfolio/prog109/lectures/js/week8JavaScript.js
@@ -0,0 +1,63 @@
+﻿var elList, addLink, newEl, newText, counter, listItems; // Declare variables
+
+
+
+
+// Create a new element and store it in a variable.
+var newEl = document.createElement('li');
+
+// Create a text node and store it in a variable.
+var newText = document.createTextNode('quinoa');
+
+// Attach the new text node to the new element.
+newEl.appendChild(newText);
+
+// Find the position where the new element should be added.
+var position = document.getElementsByTagName('ul')[0];
+
+// Insert the new element into its position.
+position.appendChild(newEl);
+
+
+function addItem(e) {                                    // Declare function
+    e.preventDefault();                                    // Prevent link action
+    newEl = document.createElement('li');                  // New <li> element
+    newText = document.createTextNode('New list item');    // New text node
+    newEl.appendChild(newText);                            // Add text to <li>
+    elList.appendChild(newEl);                             // Add <li> to list
+}
+
+
+addLink.addEventListener('click', addItem, false);       // Click on button
+elList.addEventListener('DOMNodeInserted', updateCount, false); // DOM updated
+
+
+
+
+
+
+//// Create a new element and store it in a variable.
+//var newE2 = document.createElement('li');
+
+//// Create a text node and store it in a variable.
+//var newText = document.createTextNode("newItemforlist");
+
+//// Attach the new text node to the new element.
+//newE2.appendChild(newTextLast);
+
+//// Find the position where the new element should be added.
+////var position = document.getElementsByTagName('ul')[0];
+
+//// Insert the new element into its position.
+//list.appendChild(newE2);
+
+
+
+
+    //function myFunction() {
+    //    var node = document.createElement("li");
+    //    var textnode = document.createTextNode(newItemforlist);
+    //    node.appendChild(textnode);
+    //    document.getElementById("todo").appendChild(node);
+    //}
+
